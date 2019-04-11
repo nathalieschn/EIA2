8da7de8fd73ccb8caadc54f6417abd48f9969978
Nathalie Schneider <44029026+nathalieschn@users.noreply.github.com>
2019-04-11 12:55:40 +0200

    aufgabe 3 konzept

--- Aufgabe3/js/main.js
+++ Aufgabe3/js/main.js
@@ -1,85 +1,67 @@
 /*
-Aufgabe: <Aufgabe 2 Mau Mau>
+Aufgabe: <Aufgabe 3 Mau Mau>
 Name: <Nathalie Schneider>
 Matrikel: <>
-Datum: <07.04.2019>
+Datum: <11.04.2019>
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.*/
-let anzahl;
-function anzahlHandkarten() {
-    anzahl = prompt("Anzahl der Handkarten eingeben");
-    let y = anzahl;
-}
-anzahlHandkarten();
-let alleKarten = [];
-let werte = [" 7", " 8", " 9", " 10", " Bube", " Dame", " König", " As"];
-let zeichen;
-function karteErstellen() {
-    for (let i = 0; i <= 32; i++) {
-        if (i < 8) {
-            zeichen = "Herz" + werte[i];
-            alleKarten.splice(0, 0, werte[i]);
-            let prodElement = document.createElement('div');
-            let karte = `<p> ${zeichen} </p>
-    `;
-            prodElement.innerHTML = karte;
-            document.getElementById("Handkarten").appendChild(prodElement);
-        }
-        else if (i >= 8 && i < 16) {
-            zeichen = "Kreuz" + werte[i % 8];
-            alleKarten.splice(0, 0, werte[i % 8]);
-            let prodElement = document.createElement('div');
-            let karte = `<p> ${zeichen} </p>
-        `;
-            prodElement.innerHTML = karte;
-            document.getElementById("Handkarten").appendChild(prodElement);
-        }
-        else if (i >= 16 && i < 24) {
-            zeichen = "Karo" + werte[i % 8];
-            alleKarten.splice(0, 0, werte[i % 8]);
-            let prodElement = document.createElement('div');
-            let karte = `<p> ${zeichen} </p>
-        `;
-            prodElement.innerHTML = karte;
-            document.getElementById("Handkarten").appendChild(prodElement);
-        }
-        else if (i >= 24 && i < 24) {
-            zeichen = "Pik" + werte[i % 8];
-            alleKarten.splice(0, 0, werte[i % 8]);
-            let prodElement = document.createElement('div');
-            let karte = `<p> ${zeichen} </p>
-        `;
-            prodElement.innerHTML = karte;
-            document.getElementById("Handkarten").appendChild(prodElement);
-        }
-    }
-}
-function kartenVerteilen() {
+var A03;
+(function (A03) {
+    let alleKarten = [];
     let handkarten = [];
-    for (let x = parseFloat(anzahl); handkarten.length <= x - 1; x++) {
-        let y = 0;
-        y = Math.floor((Math.random() * alleKarten.length));
-        y = y - 1;
-        handkarten.push(alleKarten[y]);
-        let prodElement = document.createElement('div');
-        let karte = `
-            <p class="${alleKarten[y]}">${alleKarten[y]}</p>            `;
-        prodElement.innerHTML = karte;
-        document.getElementById("Handkarten").appendChild(prodElement);
-        alleKarten.splice(y, 1);
+    let werte = [" 7", " 8", " 9", " 10", " Bube", " Dame", " König", " As"];
+    let zeichen;
+    let y;
+    let anzahl;
+    function anzahlHandkarten() {
+        anzahl = prompt("Anzahl der Handkarten eingeben");
+        y = parseInt(anzahl);
     }
-}
-function erstelleNachziehstapel() {
-    for (let z = 0; z < alleKarten.length; z++) {
-        let prodElement = document.createElement('div');
-        document.getElementById("Spielkarten").appendChild(prodElement);
-        let karte = `
-                <p class="${alleKarten[z]}">${alleKarten[z]}</p>
-                `;
-        prodElement.innerHTML = karte;
+    anzahlHandkarten();
+    function karteErstellen() {
+        for (let i = 0; i <= 32; i++) {
+            if (i < 8) {
+                zeichen = "Herz" + werte[i];
+                alleKarten.splice(0, 0, zeichen);
+            }
+            else if (i >= 8 && i < 16) {
+                zeichen = "Kreuz" + werte[i % 8];
+                alleKarten.splice(0, 0, zeichen);
+            }
+            else if (i >= 16 && i < 24) {
+                zeichen = "Karo" + werte[i % 8];
+                alleKarten.splice(0, 0, zeichen);
+            }
+            else if (i >= 24 && i < 32) {
+                zeichen = "Pik" + werte[i % 8];
+                alleKarten.splice(0, 0, zeichen);
+            }
+        }
     }
-}
-erstelleNachziehstapel();
-karteErstellen();
-kartenVerteilen();
+    function kartenVerteilen() {
+        for (let x = 0; x < y; x++) {
+            let randomCard = Math.floor((Math.random() * alleKarten.length));
+            handkarten.push(alleKarten[randomCard]);
+            let prodElement = document.createElement('div');
+            let karte = `<p class="${alleKarten[randomCard]}">${alleKarten[randomCard]}</p>`;
+            prodElement.innerHTML = karte;
+            document.getElementById("Handkarten").appendChild(prodElement);
+            alleKarten.splice(randomCard, 1);
+        }
+    }
+    function erstelleNachziehstapel() {
+        for (let z = 0; z <= alleKarten.length - y; z++) {
+            let prodElement = document.createElement('div');
+            document.getElementById("Nachziehstapel").appendChild(prodElement);
+            let karte = `<p class="${alleKarten[z]}">${alleKarten[z]}</p>`;
+            prodElement.innerHTML = karte;
+        }
+    }
+    function sortiereKarte() {
+        //document.addEventListener ("click",init);
+    }
+    erstelleNachziehstapel();
+    karteErstellen();
+    kartenVerteilen();
+})(A03 || (A03 = {}));
 //# sourceMappingURL=main.js.map
